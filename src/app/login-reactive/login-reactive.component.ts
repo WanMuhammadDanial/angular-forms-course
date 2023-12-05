87b6db0fad6f4b28c08925de0daa264c7ca89cb6
Wan Muhammad Danial <wmdanial12@gmail.com>
2023-12-05 22:03:20 +0800

    v24 p1

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'login',
-  templateUrl: './login-reactive.component.html',
-  styleUrls: ['./login-reactive.component.css']
+  selector: "login",
+  templateUrl: "./login-reactive.component.html",
+  styleUrls: ["./login-reactive.component.css"],
 })
 export class LoginReactiveComponent implements OnInit {
+  email = new FormControl("", {
+    validators: [Validators.required, Validators.email],
+    updateOn: "blur",
+  });
 
+  password = new FormControl("", [
+    Validators.required,
+    Validators.minLength(8),
+  ]);
 
-  constructor() {
+  form: FormGroup = new FormGroup({
+    email: this.email,
+    password: this.password,
+  });
 
+  constructor() {}
 
-  }
-
-  ngOnInit() {
-
-  }
-
+  ngOnInit() {}
 }
