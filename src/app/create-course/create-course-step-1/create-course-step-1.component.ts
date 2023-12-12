ee4c60f0f5fa7a376fd78392c3f52d1750524d10
Wan Muhammad Danial Bin Seniwan <danial.seniwan@tmrnd.com.my>
2023-12-12 15:41:33 +0800

    Vid 39

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -3,7 +3,12 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { CoursesService } from "../../services/courses.service";
 import { Observable } from "rxjs";
 import { filter } from "rxjs/operators";
+import { courseTitleValidator } from "../../validators/course-title.validator";
 
+interface CourseCategory {
+  code: string;
+  descrpition: string;
+}
 @Component({
   selector: "create-course-step-1",
   templateUrl: "./create-course-step-1.component.html",
@@ -13,11 +18,43 @@ export class CreateCourseStep1Component implements OnInit {
   form = this.fb.group({
     title: [
       "",
-      [Validators.required, Validators.minLength(5), Validators.maxLength(60)],
+      {
+        validators: [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(60),
+        ],
+        asyncValidators: [courseTitleValidator(this.courses)],
+        updateOn: "blur",
+      },
     ],
+    releasedAt: [new Date(), Validators.required],
+    category: ["BEGINNER", Validators.required],
+    downloadsAllowed: [false, Validators.requiredTrue],
+    longDescription: ["", [Validators.required, Validators.minLength(3)]],
   });
 
-  constructor(private fb: FormBuilder) {}
+  courseCategories$: Observable<CourseCategory[]>;
 
-  ngOnInit() {}
+  constructor(private fb: FormBuilder, private courses: CoursesService) {}
+
+  ngOnInit() {
+    this.courseCategories$ = this.courses.findCourseCategories();
+
+    const draft = localStorage.getItem("STEP_1");
+
+    if (draft) {
+      this.form.setValue(JSON.parse(draft));
+    }
+
+    this.form.valueChanges
+      .pipe(filter(() => this.form.valid))
+      .subscribe((val) => {
+        localStorage.setItem("STEP_1", JSON.stringify(val));
+      });
+  }
+
+  get courseTitle() {
+    return this.form.controls["title"];
+  }
 }
---
+++ src/app/validators/course-title.validator.ts
@@ -0,0 +1,19 @@
+import { map } from "rxjs/operators";
+import { AbstractControl, AsyncValidatorFn } from "@angular/forms";
+import { CoursesService } from "../services/courses.service";
+
+export function courseTitleValidator(
+  courses: CoursesService
+): AsyncValidatorFn {
+  return (control: AbstractControl) => {
+    return courses.findAllCourses().pipe(
+      map((courses) => {
+        const course = courses.find(
+          (course) =>
+            course.description.toLowerCase() == control.value.toLowerCase()
+        );
+        return course ? { titleExists: true } : null;
+      })
+    );
+  };
+}
