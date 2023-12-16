368cd7e758595bdbc9cd165211af49a3fcdc2762
Wan Muhammad Danial Bin Seniwan <danial.seniwan@tmrnd.com.my>
2023-12-16 12:53:29 +0800

    vid 52

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -6,6 +6,7 @@ import {
   ControlValueAccessor,
   NG_VALIDATORS,
   NG_VALUE_ACCESSOR,
+  ValidationErrors,
   Validator,
 } from "@angular/forms";
 import { noop, of } from "rxjs";
@@ -14,9 +15,113 @@ import { noop, of } from "rxjs";
   selector: "file-upload",
   templateUrl: "file-upload.component.html",
   styleUrls: ["file-upload.component.scss"],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: FileUploadComponent,
+    },
+    {
+      provide: NG_VALIDATORS,
+      multi: true,
+      useExisting: FileUploadComponent,
+    },
+  ],
 })
-export class FileUploadComponent {
+export class FileUploadComponent implements ControlValueAccessor, Validator {
   @Input() requiredFileType: string;
 
-  fileName: string = '';
+  fileName: string = "";
+
+  fileUploadError: boolean = false;
+
+  fileUploadSuccess: boolean = false;
+
+  uploadProgress: number;
+
+  onChange: Function = (fileName: string) => {};
+
+  onTouched: Function = () => {};
+
+  onValidatorChange = () => {};
+
+  disabled: boolean = false;
+
+  constructor(private http: HttpClient) {}
+
+  onClick(fileUpload: HTMLInputElement) {
+    this.onTouched();
+    fileUpload.click();
+  }
+
+  onFileSelected(event) {
+    const file: File = event.target.files[0];
+
+    if (file) {
+      this.fileName = file.name;
+    }
+    this.fileName = file.name;
+
+    const formData = new FormData();
+    formData.append("thumbnail", file);
+
+    this.fileUploadError = false;
+    this.http
+      .post("/api/thumbnail-upload", formData, {
+        reportProgress: true,
+        observe: "events",
+      })
+      .pipe(
+        catchError((error) => {
+          this.fileUploadError = true;
+          return of(error);
+        }),
+        finalize(() => {
+          this.uploadProgress = null;
+        })
+      )
+      .subscribe((event) => {
+        if (event.type == HttpEventType.UploadProgress) {
+          this.uploadProgress = Math.round(100 * (event.loaded / event.total));
+        } else if (event.type == HttpEventType.Response) {
+          this.fileUploadSuccess = true;
+          this.onChange(this.fileName);
+          this.onValidatorChange();
+        }
+      });
+  }
+
+  writeValue(value: any): void {
+    this.fileName = value;
+  }
+
+  registerOnChange(onChange: any): void {
+    this.onChange = onChange;
+  }
+
+  registerOnTouched(onTouched: any): void {
+    this.onTouched = onTouched;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    if (this.fileUploadSuccess) {
+      return null;
+    }
+
+    let errors: any = {
+      requiredFileType: this.requiredFileType,
+    };
+
+    if (this.fileUploadError) {
+      errors.uploadFailed = true;
+    }
+
+    return errors;
+  }
+
+  registerOnValidatorChange?(onValidatorChange: () => void): void {}
 }
